a07519cb5db96cb528b6ff634ab789810bc91faf
Diego Beltran <dbcaturra@gmail.com>
2019-10-01 19:22:04 -0500

    few fixes...

--- scripts/miescuela_mediacenter.sh
+++ scripts/miescuela_mediacenter.sh
@@ -1,1 +1,2 @@
-cd ~/storage/shared/nms && node app.js
+#cd ~/storage/shared/nms && node app.js
+cd ~/storage/shared/nms/Node-Media-Server && node app.js
--- scripts/stream_video.sh
+++ scripts/stream_video.sh
@@ -1,1 +1,2 @@
 ffmpeg -re -i ~/storage/shared/miescuela-koala/static/videos/definiciones.mp4 -c copy -f flv rtmp://0.0.0.0/live/definiciones.mp4
+#ffmpeg -re -i ~/storage/shared/miescuela-koala/static/videos/definiciones.mp4 -c:v libx264 -preset superfast -tune zerolatency -c:a aac -ar 44100 -f flv rtmp://0.0.0.0/live/stream
